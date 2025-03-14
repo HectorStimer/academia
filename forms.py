d9040c492d62a322012b1266c33c73716fd51ad2
HectorStimer <hgoten007@gmail.com>
2025-03-14 15:21:41 -0300

    featureDataBase

--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SelectField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegistroAlunoForm(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=30)])
-    confirmar_senha = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=30)])
-    telefone = StringField('Telefone')
-    plano_id = SelectField('Plano', coerce=int, validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email(message='Email inválido')])
+    senha = PasswordField('Senha', validators=[DataRequired(), EqualTo('confirmar', message="As senhas devem ser iguais.")])
+    confirmar = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('senha', message="As senhas devem ser iguais.")])
+    telefone = StringField('Telefone', validators=[DataRequired(message='Precisamos de seu telefone para contato')])
     data_nascimento = DateField('Data de Nascimento', format='%Y-%m-%d')
+    plano_id = SelectField('Plano', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Registrar')
--- routes.py
+++ routes.py
@@ -9,18 +9,21 @@ def homepage():
     return render_template("index.html")
 
 
+
 @app.route("/registrarUsuario", methods=['GET', 'POST'])
 def register():
     form = RegistroAlunoForm()
-    form.plano_id.choices = [("", "Selecione um Plano")] + [(plano.id_plano, plano.nomePano) for plano in Plano.query.all()]
-
+    
+    with app.app_context():
+        form.plano_id.choices = [(plano.id_plano, plano.nomePlano) for plano in Plano.query.all()]
+    
     if form.validate_on_submit():
         novo_aluno = Aluno(
             nome=form.nome.data,
             email=form.email.data,
             telefone=form.telefone.data,
             data_nascimento=form.data_nascimento.data,
-            plano_id=form.plano_id.data,  # Obtendo o ID do plano selecionado
+            plano_id=form.plano_id.data,  
             senha=form.senha.data
         )
         db.session.add(novo_aluno)
